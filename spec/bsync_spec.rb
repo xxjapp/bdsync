74a87b8c372d13a551806c8c9c38fca4c0ba42d4
nmaeda <nmaeda@keepdata.asia>
2019-12-25 19:43:21 +0900

    add rspec

---
+++ spec/bsync_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Bsync do
+  it "has a version number" do
+    expect(Bsync::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
